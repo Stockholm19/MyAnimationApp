b2d7d43985bf8867c4611a1382659352b324527c
Abrikozs <Abrikozs@gmail.com>
2024-12-14 00:09:47 +0200

    Initial Commit

---
+++ MyAnimationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAnimationApp
+//
+//  Created by Роман Пшеничников on 14.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
